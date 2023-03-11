367a93b19c2f2cb787d0c406f696d30662e2c26c
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-03-11 10:42:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ st.snow()
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
-if a ==0:
-    st.error('Phương trình vô nghiệm')
-else: 
-    st.success(f'Phương trình có 1 nghiệm{-b/a}')
+
+if st.button('Kiểm tra'):
+    if a ==0:
+        st.error('Phương trình vô nghiệm')
+    else: 
+        st.success(f'Phương trình có 1 nghiệm{-b/a}')
