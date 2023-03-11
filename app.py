92c86548faec2fb8e03a97f928e268aa18ff4b8a
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-03-11 10:43:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import streamlit as st
 import sklearn
 import plotly
 st.balloons()
-st.snow()
+
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 
-if st.button('Kiểm tra'):
+if st.button('Giải'):
     if a ==0:
         st.error('Phương trình vô nghiệm')
     else: 
