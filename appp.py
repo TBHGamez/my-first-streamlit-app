6c274928dcec17708db02c9fb7d6a2099e37cd3e
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-03-12 20:07:28 +0700

    Update appp.py

--- appp.py
+++ appp.py
@@ -6,7 +6,9 @@ import sklearn
 filename = 'model.pickle'
 model = pickle.load(open(filename, "rb"))
 st.balloons()
+y_pred = model.predict(x)
 ii= mae(y, y_pred)
+
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 #x_new = st.number_input('blabla')
 #y_new = model.predict(x_new)
